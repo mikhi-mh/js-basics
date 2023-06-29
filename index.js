ab6e6a2cccbcaa54d6ace5e1eeec4782578626ae
mikhi-mh <hmikhilesh@gmail.com>
2023-06-29 16:35:46 +0530

    getter and setter

--- index.js
+++ index.js
@@ -25,13 +25,53 @@ let isApproved = true;
 let firstNameMine = 'Mikhilesh';
 let lastNameMine = undefined;
 let mineLastMine = null;  //// to clear
+console.log(typeof(lastNameMine));
+console.log(typeof(mineLastMine));
+console.log("\n\n")
 
+/*
+1. Primitives/Value Types
+    Number
+    String
+    Boolean
+    undefined
+    null
+    
+2. Reference Types
+    Object
+    Array
+    Function
 
+*/
 
 ////dynamic
+// we can change data types at runtime
 
 let variable = 35;
 console.log(typeof(variable));
 
 variable = 'this is sentence' ;
 console.log(typeof(variable));
+
+
+////////
+
+let person = {
+    name : 'Mikhi' , 
+    age : 30
+};
+
+console.log(person);
+console.log("\n");
+console.log(person.name);
+console.log(person.age);
+
+//getter & setter   dot notation
+person.name = "Mikhilesh";
+console.log(person.name);
+console.log("\n");
+ 
+// getter & setter    another way   square notation
+// this is used for runtime in the browser
+person['age'] = 16;
+console.log(person['age']);

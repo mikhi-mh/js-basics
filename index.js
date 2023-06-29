3e9312f9d0e869f28ec6781716f82302db37595a
mikhi-mh <hmikhilesh@gmail.com>
2023-06-29 16:23:07 +0530

    dynamic

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 console.log('Hello, World ! from JS from that seaparate file');  
+
+let name2 = "Mikhi";
+console.log(name2);
+
+let firstName = "Mikhilesh";
+let lastName = 'Hirphode';
+
+let name;
+console.log(name);
+
+const rate = 8.5;
+console.log(rate);
+
+// const rate2 = 8.5;
+// rate2 = 3.5
+// console.log(rate2);
+
+
+//variables
+
+let nameMine = 'mikhi';
+let age = 30;
+let isApproved = true;
+let firstNameMine = 'Mikhilesh';
+let lastNameMine = undefined;
+let mineLastMine = null;  //// to clear
+
+
+
+////dynamic
+
+let variable = 35;
+console.log(typeof(variable));
+
+variable = 'this is sentence' ;
+console.log(typeof(variable));
